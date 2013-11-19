26879ef81e1549e4b6468cefd45fada0fe370a1f
Miguel Guerreiro <miguel.guerreiro@gmail.com>
2013-11-19 16:12:42 +0000

    Added test controller
    Signed-off-by: Miguel Guerreiro <miguel.guerreiro@gmail.com>

---
+++ application/controllers/test.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller
+{
+	public function __construct()
+	{
+		$this->load->library('xero');
+	}
+
+	public function index()
+	{
+		// 
+	}
+}
